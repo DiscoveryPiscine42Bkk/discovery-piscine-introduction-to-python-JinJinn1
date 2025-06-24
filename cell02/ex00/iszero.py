9bafa00cd3eaf1cf9620879ebcdae003b7990796
JinJinn1 <jin11june@gmail.com>
2025-06-24 19:36:55 +0700

    Add files via upload

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+enter_number = input("Enter a number: ").strip()
+
+if enter_number == "0":
+    print("This number is equal to zero.")
+else:
+    print("This number is different from zero.")
