e01b36827470a11bdd31e420b0be8ca998a758bc
JinJinn1 <jin11june@gmail.com>
2025-06-24 20:53:19 +0700

    password.py

---
+++ cell02/ex02/password.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+security = input("What's the password(case sensitive): ")
+
+password = "Python is awesome"
+
+if security == password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED.")
