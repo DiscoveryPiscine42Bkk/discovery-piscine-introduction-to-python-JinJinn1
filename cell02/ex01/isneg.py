6fc82cdf8d6fadb9a73e92dbcc97dd430a51d0db
JinJinn1 <jin11june@gmail.com>
2025-06-24 20:40:32 +0700

    isneg.py

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,13 @@
+enter_number = input("Please enter an integer: ").strip()
+
+final_number = int(enter_number)
+
+if final_number > 0:
+    print("This number is positive.")
+
+if final_number < 0:
+    print("This number is negative.")
+
+else:
+    print("This number is both positive and negative.")
+    
