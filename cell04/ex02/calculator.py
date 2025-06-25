cf4b2d0061e3b8cde3d92d7b2659241875df75d2
JinJinn1 <jin11june@gmail.com>
2025-06-25 17:00:21 +0700

    calculator.py

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+first = int(input("Give me the first number: "))
+second = int(input("Give me the second number: "))
+result1 = first + second
+result2 = first - second
+result3 = first*second
+result4 = first/second
+
+print("Thank you!")
+
+print(f"{first} + {second} = {result1}")
+print(f"{first} - {second} = {result2}")
+print(f"{first} * {second} = {result3}")
+print(f"{first} / {second} = {result4}")
